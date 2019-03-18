9f86b0d8cec756186d55dae57917ae2205ed0e9a
asvcode <avirdee@aol.com>
2019-03-18 13:44:55 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -90,7 +90,7 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction), '1st:' {preds_best3[0]}})
+    return JSONResponse({'result': str(prediction), '1st:' (preds_best3[0])})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
