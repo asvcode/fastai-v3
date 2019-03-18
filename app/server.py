b82d8a5f18e921868886c7ce628fc266737f7607
asvcode <avirdee@aol.com>
2019-03-18 09:37:51 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -21,10 +21,10 @@ export_file_name = 'json.pkl'
 with open('app/static/Test30.json', 'r') as f:
     cat_to_name = json.load(f)
 
-class_names = data.classes
+#class_names = data.classes
 
-for i in range(0,len(class_names)):
-    class_names[i] = cat_to_name.get(class_names[i])
+#for i in range(0,len(class_names)):
+#    class_names[i] = cat_to_name.get(class_names[i])
 
 #classes = ['beige', 'black', 'blue', 'brown', 'capsule', 'gold', 'green', 'grey', 'orange', 'pink', 'purple', 'red', 'tablet', 'tan', 'white', 'yellow']
 classes = ['Venalfaxine 37.5mg', 'Venalfaxine ER 75mg', 'Venalfaxine ER 150mg', 'Levothyroxine 25mcg', 'Levothyroxine 50mcg', 'Levothyroxine 75mcg', 'Levothyroxine 100mcg', 'Levothyroxine 112mcg', 'Omeprazole 20mg', 'Lisinopril 5mg', 'Lisinopril 10mg', 'Lisinopril 20mg', 'Atorvastatin 10mg', 'Atorvastatin 20mg', 'Atorvastatin 40mg', 'Duloxetine 20mg', 'Duloxetine 30mg', 'Duloxetine 60mg', 'Levoxyl 25mcg', 'Levoxyl 50mcg', 'Levoxyl 88mcg', 'Levoxyl 112mcg', 'Gabapentin 100mg', 'Gabapentin 300mg', 'Sertraline 25mg', 'Sertraline 50mg', 'Sertraline 100mg', 'Gabapentin 600mg', 'Gabapentin 800mg', 'Omeprazole 40mg']
