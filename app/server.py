4cab55fc1f0b92998d6dbacafdab860f2ae7feec
asvcode <avirdee@aol.com>
2019-03-20 12:00:24 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -10,8 +10,10 @@ from fastai import *
 from fastai.vision import *
 
 #AWS
-export_file_url = 'https://www.dropbox.com/s/xhoz8abah0tx7eo/resnet_one_2_0319_AWS.pkl?dl=1'
-export_file_name = 'resnet_one_2_0319_AWS.pkl'
+#export_file_url = 'https://www.dropbox.com/s/xhoz8abah0tx7eo/resnet_one_2_0319_AWS.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/2nb6cc5y98lan1q/resnet152_0320.pkl?dl=1'
+#export_file_name = 'resnet_one_2_0319_AWS.pkl'
+export_file_name= 'resnet152_0320.pkl'
 
 #Working Names
 #export_file_url = 'https://www.dropbox.com/s/yce5otqijrpfs8o/pill_3.pkl?dl=1'
