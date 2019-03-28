157a145c922dd4d4de4eb3c190a316bf0e5a2f95
asvcode <avirdee@aol.com>
2019-03-27 22:09:10 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -9,9 +9,13 @@ import json
 from fastai import *
 from fastai.vision import *
 
-#PTH upload
-export_file_url = 'https://www.dropbox.com/s/ubtyr33aa2tkvzk/DenseNet201_1_0322.pth?dl=1'
-export_file_name = 'DenseNet201_1_0322'
+#Densenet
+#export_file_url = 'https://www.dropbox.com/s/ubtyr33aa2tkvzk/DenseNet201_1_0322.pth?dl=1'
+#export_file_name = 'DenseNet201_1_0322'
+
+export_file_url = 'https://www.dropbox.com/s/gpw07b7vn47q3k9/squeezenet1_0_0327_B64.pth?dl=1'
+export_file_name = 'squeezenet1_0_0327_B64'
+
 classes = ['Venalfaxine 37.5mg', 'Venalfaxine ER 75mg', 'Venalfaxine ER 150mg', 'Levothyroxine 25mcg', 'Levothyroxine 50mcg', 'Levothyroxine 75mcg', 'Levothyroxine 100mcg', 'Levothyroxine 112mcg', 'Omeprazole 20mg', 'Lisinopril 5mg', 'Lisinopril 10mg', 'Lisinopril 20mg', 'Atorvastatin 10mg', 'Atorvastatin 20mg', 'Atorvastatin 40mg', 'Duloxetine 20mg', 'Duloxetine 30mg', 'Duloxetine 60mg', 'Levoxyl 25mcg', 'Levoxyl 50mcg', 'Levoxyl 88mcg', 'Levoxyl 112mcg', 'Gabapentin 100mg', 'Gabapentin 300mg', 'Sertraline 25mg', 'Sertraline 50mg', 'Sertraline 100mg', 'Gabapentin 600mg', 'Gabapentin 800mg', 'Omeprazole 40mg']
 
 #AWS
@@ -58,7 +62,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path/'models'/f'{export_file_name}.pth')
     data_bunch = ImageDataBunch.single_from_classes(path, classes, size=128).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.densenet201, pretrained=False)
+    learn = cnn_learner(data_bunch, models.squeezenet1_0, pretrained=False)
     learn.load(export_file_name)
     return learn
 
