4f0e6ff3c075a372337f68ac35bc302760a1c536
asvcode <avirdee@aol.com>
2019-03-31 21:26:49 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -66,12 +66,31 @@ async def download_file(url, dest):
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
 
+#async def setup_learner():
+#    await download_file(export_file_url, path/'models'/f'{export_file_name}.pth')
+#    data_bunch = ImageDataBunch.single_from_classes(path, classes, size=128).normalize(imagenet_stats)
+#    learn = cnn_learner(data_bunch, models.squeezenet1_0, pretrained=False)
+#    learn.load(export_file_name)
+#    return learn
+
+#loop = asyncio.get_event_loop()
+#tasks = [asyncio.ensure_future(setup_learner())]
+#learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+#loop.close()
+
+
 async def setup_learner():
-    await download_file(export_file_url, path/'models'/f'{export_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes, size=128).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.squeezenet1_0, pretrained=False)
-    learn.load(export_file_name)
-    return learn
+    await download_file(export_file_url, path/export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
