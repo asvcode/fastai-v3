4eaaaed8f5be708327d7c6fa9a358a6a486c669c
asvcode <avirdee@aol.com>
2019-04-02 09:11:39 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -16,6 +16,9 @@ export_file_name = 'cardene_sq.pkl'
 
 classes = ['Venalfaxine 37.5mg', 'Venalfaxine ER 75mg', 'Venalfaxine ER 150mg', 'Levothyroxine 25mcg', 'Levothyroxine 50mcg', 'Levothyroxine 75mcg', 'Levothyroxine 100mcg', 'Levothyroxine 112mcg', 'Omeprazole 20mg', 'Lisinopril 5mg', 'Lisinopril 10mg', 'Lisinopril 20mg', 'Atorvastatin 10mg', 'Atorvastatin 20mg', 'Atorvastatin 40mg', 'Duloxetine 20mg', 'Duloxetine 30mg', 'Duloxetine 60mg', 'Levoxyl 25mcg', 'Levoxyl 50mcg', 'Levoxyl 88mcg', 'Levoxyl 112mcg', 'Gabapentin 100mg', 'Gabapentin 300mg', 'Sertraline 25mg', 'Sertraline 50mg', 'Sertraline 100mg', 'Gabapentin 600mg', 'Gabapentin 800mg', 'Omeprazole 40mg']
 
+with open('app/static/Test30.json', 'r') as f:
+    cat_to_name = json.load(f)
+
 path = Path(__file__).parent
 
 app = Starlette()
@@ -58,7 +61,15 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction, indice, losses = learn.predict(img)
+    preds_sorted, idxs = losses.sort(descending=True)
+
+    class_names = learn.data.classes
+
+    result = (f' Model is NOT Confident: \n {prediction} {class_names} ({idsx})')
+
+
+    return JSONResponse({'result': str(result)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
