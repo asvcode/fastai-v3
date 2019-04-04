2135f3267bd53bbabe5c8d8d8435056ac72b4440
asvcode <avirdee@aol.com>
2019-04-03 22:03:55 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -76,7 +76,7 @@ async def analyze(request):
 
     info = learn.data.classes
 
-    result = {prediction}, {info}
+    #result = {prediction}, {info}
 
 
 
@@ -85,7 +85,7 @@ async def analyze(request):
     #result = (f' Model output: \n {prediction} {pred_1_class}\n {pred_1_prob} {pred_2_class} {pred_2_prob}')
 
 
-    return JSONResponse({'result': str(result)})
+    return JSONResponse({'result': str(info)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
