0fbaeb35084800f52c76a7156800d1ac1b1c6ce2
asvcode <avirdee@aol.com>
2019-04-19 11:46:11 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -14,16 +14,27 @@ import pretrainedmodels
 #export_file_url = 'https://www.dropbox.com/s/6ubzhbra6rc1zbd/cardene_sq.pkl?dl=1'
 #export_file_name = 'cardene_sq.pkl'
 
-export_file_url = 'https://www.dropbox.com/s/fjkf01oe76cspnr/squeeze_UNTRAINED_rerun_one_overfit_0415_84point5.pth?dl=1'
-export_file_name = 'squeeze_UNTRAINED_rerun_one_overfit_0415_84point5'
+export_file_url = 'https://www.dropbox.com/s/uvyf78rjdwm8f7b/resnet50_json92_40epoch_0896471.pth?dl=1'
+export_file_name = 'resnet50_json92_40epoch_0896471'
 
 #export_file_url = 'https://www.dropbox.com/s/1abrij8d4cinrts/squeeze_UNTRAINED_org_0415.pth?dl=1'
 #export_file_name = 'squeeze_UNTRAINED_org_0415'
 
 #classes = ['Venalfaxine 37.5mg', 'Venalfaxine ER 75mg', 'Venalfaxine ER 150mg', 'Levothyroxine 25mcg', 'Levothyroxine 50mcg', 'Levothyroxine 75mcg', 'Levothyroxine 100mcg', 'Levothyroxine 112mcg', 'Omeprazole 20mg', 'Lisinopril 5mg', 'Lisinopril 10mg', 'Lisinopril 20mg', 'Atorvastatin 10mg', 'Atorvastatin 20mg', 'Atorvastatin 40mg', 'Duloxetine 20mg', 'Duloxetine 30mg', 'Duloxetine 60mg', 'Levoxyl 25mcg', 'Levoxyl 50mcg', 'Levoxyl 88mcg', 'Levoxyl 112mcg', 'Gabapentin 100mg', 'Gabapentin #300mg', 'Sertraline 25mg', 'Sertraline 50mg', 'Sertraline 100mg', 'Gabapentin 600mg', 'Gabapentin 800mg', 'Omeprazole 40mg']
 
-classes = ['000937384', '000937385', '000937386', '003781800', '003781803', '003781805', '003781809', '003781811', '007812790', '435470352', '435470353', '435470354', '605052578', '605052579', '605052580', '605052995', '605052996', '605052997', '607930850', '607930851', '607930853', '607930855', '658620198',
-'658620199', '681800351', '681800352', '681800353', '684620126', '684620127', '684620397']
+#classes = ['000937384', '000937385', '000937386', '003781800', '003781803', '003781805', '003781809', '003781811', '007812790', '435470352', '435470353', '435470354', '605052578', '605052579', '605052580', '605052995', '605052996', '605052997', '607930850', '607930851', '607930853', '607930855', '658620198',
+#'658620199', '681800351', '681800352', '681800353', '684620126', '684620127', '684620397']
+classes = ['000937384', '000937385', '000937386', '003781800', '003781803', '003781805', '003781809', '003781811', '007812790',
+           '435470352', '435470353', '435470354', '605052578', '605052579', '605052580', '605052995', '605052996', '605052997',
+           '607930850', '607930851', '607930853', '607930855', '658620198', '658620199', '681800351', '681800352', '681800353',
+           '684620126', '684620127', '684620397', '000024463', '000024464', '000544179', '000544181', '000544182', '000544183',
+           '000544184', '000694220', '000711012', '000711013', '000711014', '000711015', '000711016', '000930753', '000932210',
+           '000933123', '000933125', '001725728', '001725729', '001850674', '003642337', '003781160', '003784250', '003786410',
+           '005910844', '005910900', '107020026', '107020027', '136680113', '136680114', '136680115', '433860356', '501110433',
+           '501110434', '591480006', '591480011', '620370710', '620370999', '633040693', '651620076', '651620077', '651620627',
+           '658620185', '658620448', '658620449', '659775036', '659775037', '669930060', '681800135', '681800136', '681800137',
+           '681800302', '681800303', '681800396', '681800397', '681800513', '681800517', '681800590', '681800591', '681800980',
+           '681800981', '683820022']
 
 #with open('app/static/json_test.json', 'r') as f:
 #    cat_to_name = json.load(f)
@@ -46,7 +57,7 @@ async def setup_learner():
      data_bunch = ImageDataBunch.single_from_classes(path, classes, size=296).normalize(imagenet_stats)
      #data = ImageDataBunch.from_folder(path, bs=64, size=296)
      #data.normalize(imagenet_stats)
-     learn = cnn_learner(data_bunch, models.squeezenet1_0, pretrained=False)
+     learn = cnn_learner(data_bunch, models.resnet50, pretrained=False)
      learn.load(export_file_name)
      return learn
 
@@ -69,7 +80,7 @@ async def analyze(request):
     pred_1_class, indice, losses = learn.predict(img)
     preds_sorted, idxs = losses.sort(descending=True)
 
-    with open('app/static/json_test.json', 'r') as f:
+    with open('app/static/json_92_version1.json', 'r') as f:
         cat_to_name = json.load(f)
 
     #class_to_idx = {sorted(cat_to_name)[i]: i for i in range(len(cat_to_name))}
